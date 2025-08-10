bab95c2e41aadf49de0ad25d4b611f3f5d3500ff
ilafe <ilafe.namra.hamdat@ensia.edu.dz>
2025-08-10 12:51:36 +0100

    Add authentication pages

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -9,21 +9,9 @@ import ResetPassword from "./pages/auth/ResetPassword"
 import VerifyEmail from "./pages/auth/VerifyEmail"
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<LandingPage />} />
-        <Route path="/afaq-plus" element={<AfaqPlus />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Signup />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
-        <Route path="/verify-email" element={<VerifyEmail />} />
-        {/* Add more routes here as needed */}
-      </Route>
-    </Routes>
-  )
+  return <VerifyEmail/>;
 }
+
+
 
 export default App
