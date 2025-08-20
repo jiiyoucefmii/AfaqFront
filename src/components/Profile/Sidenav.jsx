a9da990d5dc99d5cdcb0fd3dd66ff044dda4b5d3
Meryy0 <meriem.ouadfel@ensia.edu.dz>
2025-08-20 16:21:16 +0100

    nav bar

--- src/components/Profile/Sidenav.jsx
+++ src/components/Profile/Sidenav.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Outlet, Link, useLocation } from 'react-router-dom'
 import '../../assets/styles/Sidenav.css'
 import QuizImg from "../icons/QuizImg";
@@ -8,67 +8,72 @@ import OverviewImg from '../icons/OverviewImg';
 import LogOut from '../icons/LogOut'
 
 const Sidenav = () => {
-
     const location = useLocation();
     const isOverview = location.pathname.includes("/overview");
     const isSettings = location.pathname.includes("/settings");
     const isQuiz = location.pathname.includes("/quiz");
 
+    const [sidebarOpen, setSidebarOpen] = useState(false);
+
+    const toggleSidebar = () => {
+        setSidebarOpen(!sidebarOpen);
+    }
+
     return (
-        <div className="sidenav">
-            <aside className="sidebar">
-                <div className="top">
-                    <img src="src\assets\images\LogoW.svg" alt="" className='logo-afaq' />
-                    <hr className='hr-logo' />
-                </div>
+        <>
+            {/* Hamburger button */}
+            <button className="hamburger" onClick={toggleSidebar}>
+                &#9776;
+            </button>
 
-                <nav>
-                    <ul className='barr'>
-                        <li className={`nav-icon ${!isOverview && !isSettings && !isQuiz ? "active" : ""}`}>
-                            <Link to="/profile" className={`${!isOverview && !isSettings && !isQuiz ? "text-#FFC800-500" : ""}`}>
-                                <ProfileImg className='iconii' color={!isOverview && !isSettings && !isQuiz ? "#FFC800" : "white"} />
-                                <p style={{ color: !isOverview && !isSettings && !isQuiz ? "#FFC800" : "white" }}>Profile</p>
-                            </Link>
-                        </li>
+            <div className="sidenav">
+                <aside className={`sidebar ${sidebarOpen ? "open" : ""}`}>
+                    <div className="top">
+                        <img src="src/assets/images/LogoW.svg" alt="" className='logo-afaq' />
+                        <hr className='hr-logo' />
+                    </div>
 
-                        <li className={`nav-icon ${isOverview ? "active" : ""}`}>
-                            <Link to="/profile/overview">
-                                <OverviewImg className='iconii' color={isOverview ? "#FFC800" : "white"} />
-                                <p style={{ color: isOverview ? "#FFC800" : "white" }}>Overview & Statistics</p>
-                            </Link>
-                        </li>
+                    <nav>
+                        <ul className='barr'>
+                            <li className={`nav-icon ${!isOverview && !isSettings && !isQuiz ? "active" : ""}`}>
+                                <Link to="/profile">
+                                    <ProfileImg className='iconii' color={!isOverview && !isSettings && !isQuiz ? "#FFC800" : "white"} />
+                                    <p style={{ color: !isOverview && !isSettings && !isQuiz ? "#FFC800" : "white" }}>Profile</p>
+                                </Link>
+                            </li>
+                            <li className={`nav-icon ${isOverview ? "active" : ""}`}>
+                                <Link to="/profile/overview">
+                                    <OverviewImg className='iconii' color={isOverview ? "#FFC800" : "white"} />
+                                    <p style={{ color: isOverview ? "#FFC800" : "white" }}>Overview & Statistics</p>
+                                </Link>
+                            </li>
+                            <li className={`nav-icon ${isQuiz ? "active" : ""}`}>
+                                <Link to="/profile/quiz">
+                                    <QuizImg className='iconii' color={isQuiz ? "#FFC800" : "white"} />
+                                    <p style={{ color: isQuiz ? "#FFC800" : "white" }}>Quizes</p>
+                                </Link>
+                            </li>
+                            <li className={`nav-icon ${isSettings ? "active" : ""}`}>
+                                <Link to="/profile/settings">
+                                    <SettingsImg className='iconii' color={isSettings ? "#FFC800" : "white"} />
+                                    <p style={{ color: isSettings ? "#FFC800" : "white" }}>Settings</p>
+                                </Link>
+                            </li>
+                        </ul>
+                    </nav>
+                    <div className="log-out">
+                        <Link to="/profile">
+                            <LogOut className='iconii' color="white" />
+                            <p>Log out</p>
+                        </Link>
+                    </div>
+                </aside>
 
-                        <li className={`nav-icon ${isQuiz ? "active" : ""}`}>
-                            <Link to="/profile/quiz">
-                                <QuizImg className='iconii' color={isQuiz ? "#FFC800" : "white"} />
-                                <p style={{ color: isQuiz ? "#FFC800" : "white" }}>Quizes</p>
-                            </Link>
-                        </li>
-
-                        <li className={`nav-icon ${isSettings ? "active" : ""}`}>
-                            <Link to="/profile/settings">
-                                <SettingsImg className='iconii' color={isSettings ? "#FFC800" : "white"} />
-                                <p style={{ color: isSettings ? "#FFC800" : "white" }}>Settings</p>
-                            </Link>
-                        </li>
-
-
-                        {/* Add more links here */}
-                    </ul>
-                </nav>
-                <div className="log-out">
-                    <Link to="/profile">
-                        <LogOut className='iconii' color="white" />
-                        <p>Log out</p>
-                    </Link>
-                </div>
-            </aside>
-
-            <main className="content">
-                {/* Nested routes like Overview will render here */}
-                <Outlet />
-            </main>
-        </div>
+                <main className="content">
+                    <Outlet />
+                </main>
+            </div>
+        </>
     )
 }
 
