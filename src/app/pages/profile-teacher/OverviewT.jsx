e7c25b6453fd2df13cbb0d9fbfaf1bd5c1dd626d
Meryy0 <meriem.ouadfel@ensia.edu.dz>
2025-08-20 19:32:46 +0100

    overview teacher

--- src/app/pages/profile-teacher/OverviewT.jsx
+++ src/app/pages/profile-teacher/OverviewT.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../../../assets/styles/OverviewT.css'
+import CardOver from '../../../components/ui/CardOver'
+import Teachers from '../../../data/TeachersAfaq';
+import Pagination from '../../../components/Pagination';
 const OverviewT = () => {
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 3;
+  const totalPages = Math.ceil(Teachers.length / itemsPerPage);
+
+  const indexStart = (currentPage - 1) * itemsPerPage;
+  const displayedTeachers = Teachers.slice(indexStart, indexStart + itemsPerPage);
+
   return (
-    <div>
-      
+    <div className='overviewt'>
+      <p className='big-overview'>Overview</p>
+      <hr className='hr-ov' />
+      <div className="info-courses">
+        <CardOver number={16} text=" الدورات المكتملة" />
+        <CardOver number={2} text="إجمالي عدد الطلاب المسجلين" />
+        <CardOver number={550} text="مجموع ساعات التدريس" />
+        <CardOver number={90} text="عدد الدورات" />
+      </div>
+
+      <div className="teacher-courses">
+        <p className='my-courses'>الدورات الخاصة بي</p>
+        <p className='courses-over'>نظرة شاملة على موادك التعليمية</p>
+        <hr className='hr-ov2' />
+      </div>
+
+      <div className="my-course">
+        <div className="all-teachers1">
+          {displayedTeachers.map((teacher) => (
+            <div className="one-card" key={teacher.id}>
+              <img src={teacher.image} alt="teacher" />
+              <div className="white-bottom">
+                <p className="teacher-name">{teacher.name}</p>
+                <p className="module">{teacher.module}</p>
+                <button className="teacher-info">حول الأستاذ</button>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={setCurrentPage}
+        />
+      </div>
     </div>
   )
 }
